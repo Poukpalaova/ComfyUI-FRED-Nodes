d84382cb04a41342d48aeac5288d4e2b40756104
FredBisAI <141651008+Poukpalaova@users.noreply.github.com>
2024-10-11 09:06:31 -0400

    Update FRED_LoadRetinaFace.py

--- FRED_LoadRetinaFace.py
+++ FRED_LoadRetinaFace.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 from facexlib.detection import RetinaFace
 from .utils import models_dir
@@ -24,31 +23,4 @@ NODE_CLASS_MAPPINGS = {
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "FRED_LoadRetinaFace": "👑 FRED_LoadRetinaFace"
-=======
-import os
-from facexlib.detection import RetinaFace
-from .utils import models_dir
-
-class FRED_LoadRetinaFace:
-    models_dir = os.path.join(models_dir, 'facexlib')
-    @classmethod
-    def INPUT_TYPES(cls):
-        return {"required":{}}
-    
-    RETURN_TYPES = ("RETINAFACE", )
-    RETURN_NAMES = ("MODEL", )
-    FUNCTION = "load"
-    CATEGORY = "CFaceSwap"
-    def load(self):
-        from facexlib.detection import init_detection_model
-        return (init_detection_model("retinaface_resnet50", model_rootpath=self.models_dir), )
-
-# Dictionary mapping node names to their corresponding classes
-NODE_CLASS_MAPPINGS = {
-    "FRED_LoadRetinaFace": FRED_LoadRetinaFace
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "FRED_LoadRetinaFace": "👑 FRED_LoadRetinaFace"
->>>>>>> master
 }
